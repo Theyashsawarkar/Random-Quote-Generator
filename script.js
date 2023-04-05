da8a7c36a472edb59de18c1aa64a50212ce2aea5
YASH  SAWARKAR <104064673+Theyashsawarkar@users.noreply.github.com>
2023-04-05 17:10:38 +0530

    comments are added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Function to get a new random quote
 function getQuote() {
   $.ajax({
-    url: "https://api.quotable.io/random",
+    url: "https://api.quotable.io/random", // Call the random quote API
     success: function (data) {
-      $("#quote").html(data.content);
-      $("#author").html("- " + data.author);
+      // If the API call is successful, execute the following code
+      $("#quote").html(data.content); // Set the text of the quote element to the quote content
+      $("#author").html("- " + data.author); // Set the text of the author element to the author name
 
       // Generate a random background color
       var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -15,9 +17,11 @@ function getQuote() {
 }
 
 $(document).ready(function () {
-  getQuote();
+  // Execute the following code when the document is ready
+  getQuote(); // Call the getQuote() function to display a random quote
 
+  // Add an event listener to the "Show Another Quote" button
   $("#get-another-quote-button").on("click", function () {
-    getQuote();
+    getQuote(); // Call the getQuote() function to display a new random quote
   });
 });
